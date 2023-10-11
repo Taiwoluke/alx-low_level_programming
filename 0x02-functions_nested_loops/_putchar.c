bf251a9a0d80edfe13544150e1786ee868d1265e
Taiwo Oladamola <taiwoluke@gmail.com>
2023-10-10 21:33:21 -0700

    f

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -3,7 +3,7 @@
 	/**
 	 * _putchar - writes the character c to stdout
 	 * @c: The character to print
-	 *
+	 * Description: This program prints putchar
 	 * Return: On success 1.
 	 * On error, -1 is returned.
 	 */
