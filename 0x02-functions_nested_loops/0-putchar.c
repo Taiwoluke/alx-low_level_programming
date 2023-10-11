1b7b27781f770b5c1fdd17712e986b5a403c4db0
Taiwo Oladamola <taiwoluke@gmail.com>
2023-10-10 21:15:11 -0700

    f

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,22 @@
 #include "main.h"
+	/**
+	 * main - print _putchar
+	 * is written by mbah
+	 * Return: 0
+	 */
 
-/**
-*main - this will print inputed text as an output
-*
-*Return:this will return 0
-*/
-int main(void)
-{
-	char put[20] = "_putchar";
 
-	int i;
-
-	for (i = 0; i < 8; i++)
+	int main(void)
 	{
-		_putchar(put[i]);
-	}
-	_putchar(20);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
-}
+	}
+~
