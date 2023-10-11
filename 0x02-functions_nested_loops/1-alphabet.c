52a7b11c72721d173cb865092f4d98bc6b9afded
Taiwo Oladamola <taiwoluke@gmail.com>
2023-10-11 10:03:31 -0700

    main

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * prints the alphabets A_Z
+ * Return:0
+ */
+
+void print_alphabet(void)
+	{
+		char alphabet;
+
+
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+			_putchar(alphabet);
+
+
+		_putchar('\n');
+	
+	}
