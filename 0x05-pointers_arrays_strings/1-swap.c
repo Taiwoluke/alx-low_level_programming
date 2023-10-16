089ed770692067e35abc41c20fb4ffe1ae02ba1a
Taiwo Oladamola <taiwoluke@gmail.com>
2023-10-16 08:40:49 -0700

    hh

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swaps the values inside two integers
+ * @a: first integer to swap
+ * @b: second integer to swap
+ */
+void swap_int(int *a, int *b)
+{
+	int y;
+
+	y = *a;
+	*a = *b;
+	*b = m;
+}
