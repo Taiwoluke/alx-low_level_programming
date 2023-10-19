f4998380bc9ceaa6653a4d21386f26bb8821e004
Taiwo Oladamola <taiwoluke@gmail.com>
2023-10-19 14:11:48 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strncat - concatenate two strings dest and src
+ * using at most n bytes from src
+ * @dest: an input value
+ * @src: another input value
+ * @n: yet another input value
+ *
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+	dest[i] = src[j];
+	i++;
+	j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
