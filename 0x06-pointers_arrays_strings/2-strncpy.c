afcbcbaf291cf273ffa5d1515503727f7e49c61e
Taiwo Oladamola <taiwoluke@gmail.com>
2023-10-19 14:20:47 -0700

    str

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strncpy – this copies a string
+ * @dest: an input value
+ * @src: another input value
+ * @n: yet another input value
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
+	return (dest);
+}
