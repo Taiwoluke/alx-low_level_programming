9013784c527993660b0dd6c5e272048874d2e6da
Taiwo Oladamola <taiwoluke@gmail.com>
2023-10-25 11:36:15 -0700

    recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,47 +1,30 @@
 #include "main.h"
-
 /**
- * palind2 - obtains length of a
- * @a: string
- * @l: integer to count length
+ * wildcmp - Compare strings
+ * @s1: pointer to string params
+ * @s2: pointer to string params
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int palind2(char *a, int l)
-{
-	if (*a == 0)
-		return (l - 1);
-	return (palind2(a + 1, l + 1));
-}
-/**
- * palind3 - compares string vs string reverse
- * @a: string
- * @l: length
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: 0
  */
 
-int palind3(char *a, int l)
+int wildcmp(char *s1, char *s2)
 {
-	if (*a != *(a + l))
-		return (0);
-	else if (*a == 0)
-		return (1);
-	return (palind3(a + 1, l - 2));
-}
-/**
- * is_palindrome - checks if a string is a palindrome
- * @s: string to evaluate
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int is_palindrome(char *s)
-{
-	int l;
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
 
-	l = palind2(s, 0);
-	return (palind3(s, l));
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
 }
