184237bec20fcf7cf940407611cf66851cd18507
Taiwo Oladamola <taiwoluke@gmail.com>
2023-10-27 17:38:36 -0700

    silence

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,16 +1,14 @@
-#include <stdio.h>		
+#include <stdio.h>
 #include "main.h"
 /**
- * main - print the number of all arguments 
+ * main - print the number of all arguments
  * @argc: count of arguments
-		
- * @argv: vector of arguments		
- *
- * Return: Always 0 (Success)		
+ * @argv: vector of arguments
+ * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])		
-{		
-(void) argv;
+int main(int argc, char *argv[])
+{
+	(void) argv;
 	printf("%d\n", argc - 1);
-	return (0);	
+	return (0);
 }
