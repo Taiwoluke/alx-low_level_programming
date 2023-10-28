e68c18715695bed38ee97772d8217c256318fae9
Taiwo Oladamola <taiwoluke@gmail.com>
2023-10-27 17:06:19 -0700

    argname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,17 @@
-[200~int main(int argc, char *argv[])
+/**
+		
+ * main – outputs the program name
+		
+ * @argc: refers to how many arguments are in the command line
+		
+ * @argv: refers to the actual arguments in the command line
+		
+ *
+		
+ * Return: Always 0 (Success)
+		
+ */
+int main(int argc, char *argv[])
 {
 		printf("%s\n", argv[argc * 0]);
 
