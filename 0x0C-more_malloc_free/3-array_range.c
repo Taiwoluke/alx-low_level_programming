c9c847a139a211d8e4f3446fbce652727d10f921
Taiwo Oladamola <taiwoluke@gmail.com>
2023-11-04 04:50:26 -0700

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -2,27 +2,30 @@
 #include <stdlib.h>
 
 /**
-  * array_range – range of array
-  * @min: minimum value
-  * @max: maximum value
+  * array_range - ...
+  * @min: ...
+  * @max: ...
   *
   * Return: integer value
   */
 int *array_range(int min, int max)
 {
-	int *w, i = 0;
+	int *a, i = 0;
 
 	if (min > max)
 		return (NULL);
 
-	w = malloc((sizeof(int) * (max - min)) + sizeof(int));
+	a = malloc((sizeof(int) * (max - min)) + sizeof(int));
 
-	if (w == NULL)
+	if (a == NULL)
 		return (NULL);
 
 	while (min <= max)
 	{
-		w[i] = min;
+		a[i] = min;
 		i++;
 		min++;
 	}
+
+	return (a);
+}
